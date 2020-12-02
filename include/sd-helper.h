b62cd3b8de52fa4180fa5518189ee755e02755a9
Bastian Krause <bst@pengutronix.de>
2020-12-02 16:54:28 +0100

    sd-helper: minor refactoring
    Clean and improve functions:
    - update documentation, remove duplicate doc strings of non-static
      functions
    - separate variable declaration and code
    - sanity check input arguments
    - wrap long lines
    - prepare for future sd_event autoptr use by defining the cleanup
      function
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- include/sd-helper.h
+++ include/sd-helper.h
@@ -41,7 +41,7 @@ struct SDSource
  *
  * @param[in] source Glib GSource
  * @param[in] loop GMainLoop the GSource should be attached to.
- * @return 0 if success else != 0 if error
+ * @return 0 on success, value != 0 on error
  * @see https://developer.gnome.org/glib/stable/glib-The-Main-Event-Loop.html#GMainLoop
  * @see https://developer.gnome.org/glib/stable/glib-The-Main-Event-Loop.html#GSource
  */
@@ -51,10 +51,12 @@ int sd_source_attach(GSource *source, GMainLoop *loop);
  * @brief Create GSource from a sd_event
  *
  * @param[in] event Systemd event that should be converted to a Glib GSource
- * @return GSource
+ * @return the newly-created GSource
  * @see https://www.freedesktop.org/software/systemd/man/sd-event.html
  * @see https://developer.gnome.org/glib/stable/glib-The-Main-Event-Loop.html#GSource
  */
 GSource * sd_source_new(sd_event *event);
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(sd_event, sd_event_unref)
 
 #endif // __SD_HELPER_H__
