46b38cf7b9e511ba89374a5bb0bc6fd2fce804ea
Bastian Krause <bst@pengutronix.de>
2021-06-02 15:01:43 +0200

    treewide: add SPDX-FileCopyrightText to file headers
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- include/sd-helper.h
+++ include/sd-helper.h
@@ -1,5 +1,6 @@
 /**
  * SPDX-License-Identifier: LGPL-2.1-only
+ * SPDX-FileCopyrightText: 2018-2020 Lasse K. Mikkelsen <lkmi@prevas.dk>, Prevas A/S (www.prevas.com)
  *
  * Copyright (C) 2018-2020 Prevas A/S (www.prevas.com)
  *
