rauc/rauc-hawkbit-updater

commit 90fcb8dbc22352f8b2e99bb14e3dcfb0869a95a3

rauc-installer: let rauc_install() return result to allow install->download thread propagation
rauc_install() starts the install thread. In order to retrieve the
installation result, change its return type accordingly, consume it
in the download thread in on_new_software_ready_cb() and store it in
the new install_success member of on_new_software_userdata.

Note that install_success will only be set according to the
installation result in "run_once" mode, otherwise rauc_install()
returns TRUE immediately without waiting for the install thread to
finish.

Signed-off-by: Bastian Krause <bst@pengutronix.de>

include/rauc-installer.h
@@ -47,8 +47,10 @@ struct install_context {
  * @param[in] on_install_complete Callback function to be called with the result of the
  *                                installation.
  * @param[in] wait Whether to wait until install thread finished or not.
+ * @return for wait=TRUE, TRUE if installation succeeded, FALSE otherwise; for
+ *         wait=FALSE TRUE is always returned immediately
  */
-void rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
+gboolean rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
                 GSourceFunc on_install_complete, gboolean wait);
 
 #endif // __RAUC_INSTALLER_H__

src/rauc-installer.c
@@ -199,13 +199,13 @@ notify_complete:
         return NULL;
 }
 
-void rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
-                  GSourceFunc on_install_complete, gboolean wait)
+gboolean rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
+                      GSourceFunc on_install_complete, gboolean wait)
 {
         GMainContext *loop_context = NULL;
         struct install_context *context = NULL;
 
-        g_return_if_fail(bundle);
+        g_return_val_if_fail(bundle, FALSE);
 
         loop_context = g_main_context_new();
         context = install_context_new();
@@ -222,6 +222,11 @@ void rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
 
         // start install thread
         thread_install = g_thread_new("installer", install_loop_thread, (gpointer) context);
-        if (wait)
+        if (wait) {
                 g_thread_join(thread_install);
+                return context->status_result == 0;
+        }
+
+        // return immediately if we did not wait for the install thread
+        return TRUE;
 }

include/hawkbit-client.h
@@ -108,6 +108,7 @@ struct on_new_software_userdata {
         GSourceFunc install_progress_callback;  /**< callback function to be called when new progress */
         GSourceFunc install_complete_callback;  /**< callback function to be called when installation is complete */
         gchar *file;                            /**< downloaded new software file */
+        gboolean install_success;               /**< whether the installation succeeded or not (only meaningful for run_once mode!) */
 };
 
 /**

src/hawkbit-client.c
@@ -749,6 +749,7 @@ static gpointer download_thread(gpointer data)
                 .install_progress_callback = (GSourceFunc) hawkbit_progress,
                 .install_complete_callback = install_complete_cb,
                 .file = hawkbit_config->bundle_download_location,
+                .install_success = FALSE,
         };
         g_autoptr(GError) error = NULL, feedback_error = NULL;
         g_autofree gchar *msg = NULL, *sha1sum = NULL;

src/rauc-hawkbit-updater.c
@@ -122,8 +122,8 @@ static gboolean on_new_software_ready_cb(gpointer data)
 
         notify_hawkbit_install_progress = userdata->install_progress_callback;
         notify_hawkbit_install_complete = userdata->install_complete_callback;
-        rauc_install(userdata->file, on_rauc_install_progress_cb, on_rauc_install_complete_cb,
-                     run_once);
+        userdata->install_success = rauc_install(userdata->file, on_rauc_install_progress_cb,
+                                                 on_rauc_install_complete_cb, run_once);
 
         return G_SOURCE_REMOVE;
 }

commit 80710c281af9204a4651d02ee1791a25a15dde97

rauc-installer: fix install_context use-after-free on wait
The download thread calls rauc_install() which creates the installation
thread. In "run_once" operation mode, the download thread waits for the
install thread to finish. In this case the download thread reads the
installation's status result from the install_context struct.

The installation thread frees the install_context before the
download thread reads the status result. Fix that by letting the
installation thread know when to leave the freeing of the
install_context struct to the download thread.

Found with address sanitizer (-fsanitize=address).

Fixes: 90fcb8db ("rauc-installer: let rauc_install() return result to allow install->download thread propagation")
Signed-off-by: Bastian Krause <bst@pengutronix.de>

include/rauc-installer.h
@@ -20,6 +20,7 @@ struct install_context {
         gint status_result;           /**< The result of the installation */
         GMainLoop *mainloop;          /**< The installation GMainLoop  */
         GMainContext *loop_context;   /**< GMainContext for the GMainLoop */
+        gboolean keep_install_context; /**< Whether the installation thread should free this struct or keep it */
 };
 
 /**

src/rauc-installer.c
@@ -177,7 +177,10 @@ notify_complete:
 
         g_clear_pointer(&r_installer_proxy, g_object_unref);
         g_main_context_pop_thread_default(context->loop_context);
-        install_context_free(context);
+
+        // on wait, calling function will take care of freeing after reading context->status_result
+        if (!context->keep_install_context)
+                install_context_free(context);
         return NULL;
 }
 
@@ -197,6 +200,7 @@ gboolean rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
         context->mainloop = g_main_loop_new(loop_context, FALSE);
         context->loop_context = loop_context;
         context->status_result = 2;
+        context->keep_install_context = wait;
 
         // unref/free previous install thread by joining it
         if (thread_install)
@@ -205,8 +209,13 @@ gboolean rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
         // start install thread
         thread_install = g_thread_new("installer", install_loop_thread, (gpointer) context);
         if (wait) {
+                gboolean result;
+
                 g_thread_join(thread_install);
-                return context->status_result == 0;
+                result = context->status_result == 0;
+
+                install_context_free(context);
+                return result;
         }
 
         // return immediately if we did not wait for the install thread