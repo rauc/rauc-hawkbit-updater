43a4c982a503d72e1f002352f15f26b95637b14c
Bastian Krause <bst@pengutronix.de>
2022-04-21 12:34:28 +0200

    rauc-installer: pass through auth header for bundle HTTP streaming
    The authentication header needs to be passed to the InstallBundle() DBus method
    invoking the RAUC streaming installation. In order to do that, add an
    `auth_header` argument to rauc_install() and make it pass the header via
    the `install_context` struct. The header is finally passed to
    InstallBundle() [1] as "http-headers" argument.
    
    For now, `rauc_install()` is called without an actual authentication
    (NULL). This will be changed in future commit.
    
    [1] https://rauc.readthedocs.io/en/latest/reference.html#gdbus-method-de-pengutronix-rauc-installer-installbundle
    
    Signed-off-by: Bastian Krause <bst@pengutronix.de>

--- include/rauc-installer.h
+++ include/rauc-installer.h
@@ -13,6 +13,7 @@
  */
 struct install_context {
         gchar *bundle;                /**< Rauc bundle file to install */
+        gchar *auth_header;           /**< Authentication header for bundle streaming */
         GSourceFunc notify_event;     /**< Callback function */
         GSourceFunc notify_complete;  /**< Callback function */
         GMutex status_mutex;          /**< Mutex used for accessing status_messages */
@@ -27,6 +28,8 @@ struct install_context {
  * @brief RAUC install bundle
  *
  * @param[in] bundle RAUC bundle file (.raucb) to install.
+ * @param[in] auth_header Authentication header on HTTP streaming installation or NULL on normal
+ *                        installation.
  * @param[in] on_install_notify Callback function to be called with status info during
  *                              installation.
  * @param[in] on_install_complete Callback function to be called with the result of the
@@ -35,7 +38,7 @@ struct install_context {
  * @return for wait=TRUE, TRUE if installation succeeded, FALSE otherwise; for
  *         wait=FALSE TRUE is always returned immediately
  */
-gboolean rauc_install(const gchar *bundle, GSourceFunc on_install_notify,
+gboolean rauc_install(const gchar *bundle, const gchar *auth_header, GSourceFunc on_install_notify,
                 GSourceFunc on_install_complete, gboolean wait);
 
 #endif // __RAUC_INSTALLER_H__
